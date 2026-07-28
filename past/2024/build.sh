057fac8faf7798a8d4d7951c1c0fb25cbe980568
Leo Suh <candorleo02@gmail.com>
2026-07-28 23:50:29 +0900

    Delete past directory

--- past/2024/build.sh
+++
@@ -1,5 +0,0 @@
-rm -r build
-
-python setup.py build_ext --inplace
-
-#clear
--- past/2024/build_mac.sh
+++
@@ -1,7 +0,0 @@
-export CC=/opt/homebrew/bin/gcc-14
-export CXX=/opt/homebrew/bin/g++-14
-
-rm -r build
-
-python setup.py build_ext --inplace
-
